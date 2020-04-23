d624e96be4e7ef8d2a1e64e3620af6561d8fba65
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 09:19:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,32 +15,42 @@
     $b = isset($_POST['b']) ? $_POST['b'] : '';
     $c = isset($_POST['c']) ? $_POST['c'] : '';
  
-    // Bước 2: Validate và tính toán
-    $delta = ($b*$b) - (4*$a*$c);
+    // kiểm tra có phải là 1 tam giác không
+    
  
-    if ($delta < 0){
-        $result = 'Phương trình vô nghiệm';
-    }
-    else if ($delta == 0){
-        $result = 'Phương trình nghiệm kép x1 = x2 = ' . (-$b/2*$a);
-    }
-    else {
-        $result = 'Phương trình có hai nghiệm phân biệt, x1 = ' . ((-$b + sqrt($delta))/2*$a);
-        $result .= ',x2 = ' . ((-$b - sqrt($delta))/2*$a);
-    }
-            }
+    if ($a == $b && $b == $c) {
+        $tam_giac = ‘Tam giác đều’;
+}
+    
+else if ($a == $b || $a == $c || $c == $b)  {
+        if ($a2==$b2+$c2 || $b2==$a2+$c2 || $c2==$a2+$b2) {
+            $tam_giac = ‘Tam giác vuông cân’;
+        }
+else {
+            $tam_giac = ‘Tâm giác cân’;
+        }
+}
+    
+else if ($a2==$b2+$c2 || $b2==$a2+$c2 || $c2==$a2+$b2) {
+        $tam_giac= ‘Tam giác vuông’;
+}
+    
+else {
+        $tam_giac = ‘Tam giác thường’;
+}
+            
         ?>
-        <h1>Giải phương trình bậc hai</h1>
+        <h1>Phân loại tam giác</h1>
         <form method="post" action="">
-            <input type="text" style="width: 20px" name="a" value=""/>x <sup>2</sup>
-            +
-            <input type="text" style="width: 20px" name="b" value=""/>x
-            + 
-            <input type="text" style="width: 20px" name="c" value=""/>
-            = 0
+            <input type="text" style="width: 20px" name="a" value=""/>cạnh 1
+            &
+            <input type="text" style="width: 20px" name="b" value=""/>cạnh 2
+            &
+            <input type="text" style="width: 20px" name="c" value=""/>cạnh 3
+            
             <br/><br/>
-            <input type="submit" name="calculate" value="Tính" />
+            <input type="submit" name="calculate" value="phân loại" />
         </form>
-        <?php echo $result; ?>
+        <?php echo $tamgiac; ?>
     </body>
 </html>
